4faa95ee8678c469d24b502de411e7b244ef5bcc
SpikeBonjour <triphuongvasa@gmail.com>
2023-02-04 23:50:48 +0700

    change to python run

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ import os
 
 if __name__ == "__main__":
     subprocess.run("pc init", shell=True)
+    subprocess.run("apt install npm", shell=True)
+    subprocess.run("npm install -g next", shell=True)
     subprocess.run(f"pc run --port {os.environ['PORT']}", shell=True)
