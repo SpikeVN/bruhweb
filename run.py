2e8a3a8ed04442f2201ad54f573dbd0eaf0ef33e
SpikeBonjour <triphuongvasa@gmail.com>
2023-02-04 23:59:58 +0700

    prod mode

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 if __name__ == "__main__":
     subprocess.run("pc init", shell=True)
     subprocess.run("apt install npm", shell=True)
-    subprocess.run("npm install -g next", shell=True)
-    subprocess.run(f"pc run --port {os.environ['PORT']}", shell=True)
+    subprocess.run("npm install -g next @chakra-ui/descendant", shell=True)
+    subprocess.run(f"pc run --port {os.environ['PORT']} --env prod", shell=True)
