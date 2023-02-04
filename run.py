9a86d7285d12e38f9b8735a0316f63776a077d56
SpikeBonjour <triphuongvasa@gmail.com>
2023-02-04 23:46:04 +0700

    change to python run

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import subprocess
+import os
 
 if __name__ == "__main__":
     subprocess.run("pc init", shell=True)
-    subprocess.run("pc run --env prod", shell=True)
+    subprocess.run(f"pc run --port {os.environ['PORT']}", shell=True)
