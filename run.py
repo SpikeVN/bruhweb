801809e5f223f75d3ed279318ed66d03af94eafd
SpikeBonjour <triphuongvasa@gmail.com>
2023-02-05 09:30:24 +0700

    change to python run

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import subprocess
 import os
 
 if __name__ == "__main__":
+    subprocess.run("pip3 install -r requirements.txt", shell=True)
+    subprocess.run("curl -fsSL https://deb.nodesource.com/setup_16.x | bash -", shell=True)
+    subprocess.run("apt install -y nodejs", shell=True)
     subprocess.run("pc init", shell=True)
     subprocess.run("apt install npm", shell=True)
     subprocess.run("npm install -g next @chakra-ui/descendant", shell=True)
